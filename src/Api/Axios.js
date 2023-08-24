644f7692cd3a67b197031dbc6e49385ef31dace6
Mohammed irfad <muhammedirfad43@gmail.com>
2023-08-24 10:40:35 +0530

    completed

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axiosInstance = Axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://uniconnect-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
